3442c65bd9d09a5b7e6ec220bd51532395a9c440
Gerónimo <gbazanpereyra@gmail.com>
2021-02-04 10:58:05 -0300

    Primer Commit

---
+++ src/main/java/com/assassins/todoroto/TodoRotoApplication.java
@@ -0,0 +1,13 @@
+package com.assassins.todoroto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoRotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoRotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assassins/todoroto/TodoRotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assassins.todoroto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoRotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
