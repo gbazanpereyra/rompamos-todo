6e16ee0f523e3d0e203e293ef166cee08bcfc5c3
Gerónimo <gbazanpereyra@gmail.com>
2021-02-05 09:50:36 -0300

    Nuestro controller

---
+++ src/main/java/com/assassins/todoroto/controllers/FrancoGeroController.java
@@ -0,0 +1,23 @@
+package com.assassins.todoroto.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController("/")
+public class FrancoGeroController {
+
+    @GetMapping("/franco-gero")
+    public ResponseEntity<?> francoGero() {
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("mensaje", "que haces wex");
+
+        return new ResponseEntity<Map>(response, HttpStatus.OK);
+    }
+
+}
