43dc5fae929333fb1e321c3904dbdf343e1725c5
Martina Ferrari <martinaferrari98@gmail.com>
2021-02-09 10:17:41 -0300

    Rompe Martina pummm

--- src/main/java/com/assassins/todoroto/entities/Cliente.java
+++ src/main/java/com/assassins/todoroto/entities/Cliente.java
@@ -39,6 +39,6 @@ public class Cliente {
 
     @Override
     public String toString() {
-        return "Cliente: |[" + "nombre = '" + nombre + "'; apellido = '" + apellido + "']|";
+        return "Cliente: |[" + "nombreMartina = '" + nombre + "'; apellidoFerrari = '" + apellido + "']|";
     }
 }
